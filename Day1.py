b0f9301d9bde72fc6dbcb417aa151571cfe05840
ujwalshet21 <ujwalshet21@gmail.com>
2026-02-23 11:24:24 +0530

    Updated with another name

---
+++ Day1.py
@@ -0,0 +1,9 @@
+print("HIIIIIIIIIIIIIIIIIIIIIII UJWALLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL\n\n\n\n\n\n\n\n\n")
+a=int(input("enter value of num1:"))
+b=int(input("Enter value for num2:"))
+print("Addition of num1 and num2 is",+a+b)
+Num3=10
+Num4=2
+print("Addition of num1 and num2 is",Num3+Num4)
+
+
